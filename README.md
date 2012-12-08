907e5e024d24682091570e1170c60be337881271
Silas Snider <swsnider@gmail.com>
2012-12-07 23:40:54 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Firmata.py
+
+## Overview
+
+Firmata.py presents an easy-to-use API for the firmata wire protocol commonly used with arduino boards. Firmata.py implements the entire protocol (including things like the Capabilities query and I2C).
+
+Firmata.py is licensed under the MIT License, a copy of which is present in the LICENSE file included in this distribution.
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name = "firmata.py",
+  version = "0.1",
+  author = "Silas Snider",
+  author_email = "swsnider@gmail.com",
+  description = ("An API wrapper for the firmata wire protocol."),
+  license = "MIT",
+  keywords = "firmata arduino serial",
+  url = "https://github.com/swsnider/firmata",
+  packages=['firmata', 'tests'],
+  long_description=read('README'),
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Topic :: Utilities",
+    "License :: OSI Approved :: MIT License",
+  ],
+  install_requires=[
+    "pyserial>=2.6"
+  ]
+)
