b99297fa8c1ac54ebef3406fba51786cb7a7f7f6
Silas Snider <swsnider@gmail.com>
2012-12-11 20:11:23 -0800

    Change code to reflect practical experience

--- firmata/__init__.py
+++ firmata/__init__.py
@@ -11,6 +11,7 @@ go to space today if you create more than on on the same serial port.
 
 from Queue import Queue, Empty
 import serial
+import sys
 import threading
 
 from firmata.constants import *
@@ -19,31 +20,55 @@ IO_TIMEOUT = 0.2  # Number of seconds to block on IO. Set to None for infinite t
 BYTES_IN_CHUNK = 100
 
 class _IOThread(threading.Thread):
-  def __init__(self, port, baud):
+  def __init__(self, port, baud, log=False):
     self._port = port
     self._baud = baud
     self.shutdown = False
     self.from_board = Queue()
+    self.from_board_condition = threading.Condition()
     self.to_board = Queue()
+    self._log = log
     super(_IOThread, self).__init__()
 
   def run(self):
+    """Thread that communicates over the serial port.
+
+    It operates by reading at most BYTES_IN_CHUNK bytes into the read queue, then writing at most BYTES_IN_CHUNK
+    bytes from the write queue. Since it uses task_done() after each written byte, it's possible to .join() the
+    write queue from the main thread to block until a write completes.
+    """
     serial_port = serial.Serial(port=self._port, baudrate=self._baud, timeout=0.2)
+    logfile = None
+    if self._log:
+      logfile = open('serial_log.txt', 'w')
+    serial_port.flushInput()
+    serial_port.flushOutput()
     while not self.shutdown:
+      # The stuff with self.from_board_condition serves the purpose of notifying the main thread when there are things
+      # to read, if the main thread was trying to block on reading these things.
+      self.from_board_condition.acquire()
       r = serial_port.read(BYTES_IN_CHUNK)
-      [self.from_board.put(i) for i in r]
+      while len(r) != 0:
+        [(self.from_board.put(ord(i)), logfile.write('<< %s (%s)\n' % (hex(ord(i)), CONST_R.get(ord(i), ''))) if self._log else None) for i in r]
+        if self._log:
+          logfile.flush()
+        r = serial_port.read(BYTES_IN_CHUNK)
+      self.from_board_condition.notifyAll()
+      self.from_board_condition.release()
       bytes_written = 0
       while not self.to_board.empty() and bytes_written < BYTES_IN_CHUNK:
         try:
           w = self.to_board.get(block=False)
-          serial_port.write(w)
+          (serial_port.write(chr(w)), logfile.write('>> %s (%s)\n' % (hex(w), CONST_R.get(w, ''))) if self._log else None)
           self.to_board.task_done()
           bytes_written += 1
         except Empty:
           break
+    serial_port.close()
+    logfile.close()
 
 class Board(object):
-  def __init__(self, port, baud, start_serial=False):
+  def __init__(self, port, baud, log=False, start_serial=False):
     """Board object constructor. Should not be called directly.
 
     Args:
@@ -51,13 +76,35 @@ class Board(object):
       baud: A number representing the baud rate to use for serial communication.
       start_serial: If True, starts the serial IO thread right away. Default: False.
     """
-    self._io_thread = _IOThread(port, baud)
-    if start_serial:
-      self._io_thread.start()
+    self._io_thread = _IOThread(port, baud, log=log)
     self._out = self._io_thread.to_board
     self._in = self._io_thread.from_board
+    self._in_condition = self._io_thread.from_board_condition
+    if start_serial:
+      self._io_thread.start()
 
-def FirmataInit(port, baud=57600):
+  def Init(self):
+    """Start serial port communications, and configure ourselves by processing a capability query."""
+    try:
+      [self._out.put(i) for i in (CONST['SYSEX_START'], CONST['SE_CAPABILITY_QUERY'], CONST['SYSEX_END'])]
+      self._out.join()
+      while True:
+        self._in_condition.acquire()  # In effect, this turns on cooperative multi-tasking with the IO thread.
+        inp = None
+        try:
+          inp = self._in.get(block=False)
+          print '%s (%s)' % (hex(inp), CONST_R.get(inp, ''))
+        except Empty:
+          self._in_condition.wait()
+          continue
+        self._in_condition.release()
+    except:
+      self._io_thread.shutdown = True
+      self._io_thread.join()
+      raise
+
+
+def FirmataInit(port, baud=57600, log=False):
   """Instantiate a `Board` object for a given serial port.
 
   Args:
@@ -67,4 +114,4 @@ def FirmataInit(port, baud=57600):
   Returns:
     A Board object which implements the firmata protocol over the specified serial port.
   """
-  return Board(port, baud, start_serial=True)
+  return Board(port, baud, log=log, start_serial=True)
--- firmata/constants.py
+++ firmata/constants.py
@@ -1,31 +1,37 @@
 """Constants used by the Firmata wire protocol."""
 
-ANALOG_MESSAGE = 0xE0
-DIGITAL_MESSAGE = 0x90
-REPORT_ANALOG = 0xC0
-REPORT_DIGITAL = 0xD0
-SYSEX_START = 0xF0
-SET_PIN_MODE = 0xF4
-SYSEX_END = 0xF7
-PROTOCOL_VERSION = 0xF9
-SYSTEM_RESET = 0xFF
+CONST = dict(
+  ANALOG_MESSAGE = 0xE0,
+  DIGITAL_MESSAGE = 0x90,
+  REPORT_ANALOG = 0xC0,
+  REPORT_DIGITAL = 0xD0,
+  SYSEX_START = 0xF0,
+  SET_PIN_MODE = 0xF4,
+  SYSEX_END = 0xF7,
+  PROTOCOL_VERSION = 0xF9,
+  SYSTEM_RESET = 0xFF,
 
-# SYSEX Commands
-SE_RESERVED_COMMAND = 0x00 # 2nd SysEx data byte is a chip-specific command (AVR, PIC, TI, etc).
-SE_ANALOG_MAPPING_QUERY = 0x69 # ask for mapping of analog to pin numbers
-SE_ANALOG_MAPPING_RESPONSE = 0x6A # reply with mapping info
-SE_CAPABILITY_QUERY = 0x6B # ask for supported modes and resolution of all pins
-SE_CAPABILITY_RESPONSE = 0x6C # reply with supported modes and resolution
-SE_PIN_STATE_QUERY = 0x6D # ask for a pin's current mode and value
-SE_PIN_STATE_RESPONSE = 0x6E # reply with a pin's current mode and value
-SE_EXTENDED_ANALOG = 0x6F # analog write (PWM, Servo, etc) to any pin
-SE_SERVO_CONFIG = 0x70 # set max angle, minPulse, maxPulse, freq
-SE_STRING_DATA = 0x71 # a string message with 14-bits per char
-SE_SHIFT_DATA = 0x75 # shiftOut config/data message (34 bits)
-SE_I2C_REQUEST = 0x76 # I2C request messages from a host to an I/O board
-SE_I2C_REPLY = 0x77 # I2C reply messages from an I/O board to a host
-SE_I2C_CONFIG = 0x78 # Configure special I2C settings such as power pins and delay times
-SE_REPORT_FIRMWARE = 0x79 # report name and version of the firmware
-SE_SAMPLING_INTERVAL = 0x7A # sampling interval
-SE_SYSEX_NON_REALTIME = 0x7E # MIDI Reserved for non-realtime messages
-SE_SYSEX_REALTIME = 0x7F # MIDI Reserved for realtime messages
+  # SYSEX Commands
+  SE_RESERVED_COMMAND = 0x00, # 2nd SysEx data byte is a chip-specific command (AVR, PIC, TI, etc).
+  SE_ANALOG_MAPPING_QUERY = 0x69, # ask for mapping of analog to pin numbers
+  SE_ANALOG_MAPPING_RESPONSE = 0x6A, # reply with mapping info
+  SE_CAPABILITY_QUERY = 0x6B, # ask for supported modes and resolution of all pins
+  SE_CAPABILITY_RESPONSE = 0x6C, # reply with supported modes and resolution
+  SE_PIN_STATE_QUERY = 0x6D, # ask for a pin's current mode and value
+  SE_PIN_STATE_RESPONSE = 0x6E, # reply with a pin's current mode and value
+  SE_EXTENDED_ANALOG = 0x6F, # analog write (PWM, Servo, etc) to any pin
+  SE_SERVO_CONFIG = 0x70, # set max angle, minPulse, maxPulse, freq
+  SE_STRING_DATA = 0x71, # a string message with 14-bits per char
+  SE_SHIFT_DATA = 0x75, # shiftOut config/data message (34 bits)
+  SE_I2C_REQUEST = 0x76, # I2C request messages from a host to an I/O board
+  SE_I2C_REPLY = 0x77, # I2C reply messages from an I/O board to a host
+  SE_I2C_CONFIG = 0x78, # Configure special I2C settings such as power pins and delay times
+  SE_REPORT_FIRMWARE = 0x79, # report name and version of the firmware
+  SE_SAMPLING_INTERVAL = 0x7A, # sampling interval
+  SE_SYSEX_NON_REALTIME = 0x7E, # MIDI Reserved for non-realtime messages
+  SE_SYSEX_REALTIME = 0x7F, # MIDI Reserved for realtime messages
+)
+
+globals().update(CONST)
+
+CONST_R = {v:k for (k,v) in CONST.items()}
