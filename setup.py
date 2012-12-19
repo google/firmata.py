a18154a266f74a39c14ccb812409ab617fb5dde5
Silas Snider <swsnider@gmail.com>
2012-12-19 12:28:44 -0800

    Update versiom number for setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
   name = "firmata.py",
-  version = "0.1.1",
+  version = "0.1.2",
   author = "Silas Snider",
   author_email = "swsnider@gmail.com",
   description = ("An API wrapper for the firmata wire protocol."),
