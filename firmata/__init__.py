4d4c5256f5f199aa65928fd110f44eef1357937c
Matthew Wilson <matthew@shinythings.com>
2012-12-31 23:01:09 -0800

    Add QueryBoardState() and QueryPinState() methods

--- firmata/__init__.py
+++ firmata/__init__.py
@@ -251,10 +251,35 @@ class Board(threading.Thread):
     wait_analog_mapping_response.wait()
     wait_analog_mapping_response.release()
 
-  def QueryBoardState(self):
+  def QueryBoardState(self, wait=True):
+    """ Query the state of all pins and update board accordingly."""
     if not self.pin_config and self.dtoa_map:
       self.QueryBoardParameters()
-    # Send SE_PIN_STATE_QUERY for each pin, wait til all return
+    if not wait:
+      for i in xrange(len(self.pin_config)):
+        self.QueryPinState(i)
+    else:
+      pin_count_lock = threading.Condition()
+      pin_count_lock.acquire()
+      pin_count = len(self.pin_config)
+
+      def PinStateResponseListener(token):
+        pin_count_lock.acquire()
+        pin_count -= 1
+        pin_count_lock.notify_all()
+        pin_count_lock.release()
+        return (True, False)
+
+      for i in xrange(len(self.pin_config)):
+        self.AddListener('PIN_STATE_RESPONSE', PinStateResponseListener)
+        self.QueryPinState(i)
+        pin_count_lock.wait()
+        pin_count_lock.release()
+      assert pin_count_lock != 0
+
+  def QueryPinState(self, pin):
+    assert 0 <= pin < len(self.pin_config)
+    self.SendSysex(SE_PIN_STATE_QUERY, [pin])
 
   def QueryCapabilities(self):
     self.SendSysex(SE_CAPABILITY_QUERY)
@@ -344,7 +369,7 @@ def FirmataInit(port, baud=57600, log_to_file=None):
     A Board object which implements the firmata protocol over the specified serial port.
   """
   board = Board(port, baud, log_to_file=log_to_file, start_serial=True)
-  board.QueryBoardParameters()
+  board.QueryBoardState()
   return board
 
 __all__ = ['FirmataInit', 'Board', 'SerialPort'] + CONST_R.values()
