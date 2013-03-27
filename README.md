904a1c53d603a1e7222356b49f956cc403d7d227
Silas Snider <swsnider@gmail.com>
2013-03-27 16:34:30 -0700

    fix licensing

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 Firmata.py presents an easy-to-use API for the firmata wire protocol commonly used with arduino boards. Firmata.py implements the entire protocol (including things like the Capabilities query and I2C).
 
-Firmata.py is licensed under the MIT License, a copy of which is present in the LICENSE file included in this distribution.
+Firmata.py is licensed under the Apache License, a copy of which is present in the LICENSE file included in this distribution.
--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 from setuptools import setup
 
@@ -17,7 +30,7 @@ setup(
   author = "Silas Snider",
   author_email = "swsnider@gmail.com",
   description = ("An API wrapper for the firmata wire protocol."),
-  license = "MIT",
+  license = "APACHE",
   keywords = "firmata arduino serial",
   url = "https://github.com/swsnider/firmata.py",
   packages=['firmata', 'tests'],
@@ -25,7 +38,7 @@ setup(
   classifiers=[
     "Development Status :: 3 - Alpha",
     "Topic :: Utilities",
-    "License :: OSI Approved :: MIT License",
+    "License :: OSI Approved :: Apache Software License",
   ],
   install_requires=[
     "pyserial>=2.6",
